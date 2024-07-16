d17313fa8abd4630e3441227cb7eee1c3e3f6e4e
phnorwood <patrick.h.norwood@gmail.com>
2024-07-16 11:42:56 -0400

    remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -3,13 +3,13 @@
 
 terraform {
 
-  cloud {
-    organization = "organization-name"
+  /*cloud {
+    organization = "porwood-dot-com"
 
     workspaces {
       name = "learn-terraform"
     }
-  }
+  }*/
 
   required_providers {
     aws = {
